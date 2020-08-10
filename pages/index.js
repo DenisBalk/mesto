d28b579564d28c3fedf1b04896f0959c608c38c8
Denis Daleckiy <balkoonski@gmail.com>
2020-08-11 00:29:30 +0300

    F

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+const openModalButton = document.querySelector('.profile__edit-button');
+const closeModalButton = document.querySelector('.modal__close-button');
+const modal = document.querySelector('.modal');
+const form = document.querySelector('.modal__form');
+
+const inputName = document.querySelector('.modal__input_type_name');
+const inputStatus = document.querySelector('.modal__input_type_status');
+
+const profileName = document.querySelector('.profile__title');
+const profileText = document.querySelector('.profile__text');
+
+function toggleModal() {
+
+  if (!modal.classList.contains('modal_is-open')) {
+    inputName.value = profileName.textContent;
+    inputStatus.value = profileText.textContent;
+  }
+
+  modal.classList.toggle('modal_is-open');
+}
+openModalButton.addEventListener('click', toggleModal);
+closeModalButton.addEventListener('click', toggleModal);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileText.textContent = inputStatus.value;
+
+  toggleModal();
+
+})
+
+
+
